6ad7770ed5cb6dd9b2a94c5881d513dd07eeb5a7
luciladayan <163851662+luciladayan@users.noreply.github.com>
2024-06-24 09:12:07 -0300

    terminado sin colores

---
+++ Assets/SCRIPTBANCO.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCRIPTBANCO : MonoBehaviour
+{
+    public GameObject[] objetos;
+    public int currentObjectIndex = -1;
+
+    void Start()
+    {
+        DeactivateAllObjects();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ActivatePreviousObject();
+        }
+        else if ((Input.GetKeyDown(KeyCode.RightArrow)))
+        {
+            ActivateNextObject();
+        }
+
+    }
+
+    public void ActivateNextObject()
+    {
+        currentObjectIndex++;
+        if (currentObjectIndex >= objetos.Length)
+        {
+            currentObjectIndex = 0;
+        }
+        DeactivateAllObjects();
+        objetos[currentObjectIndex].SetActive(true);
+    }
+
+    public void ActivatePreviousObject()
+    {
+        currentObjectIndex--;
+        if (currentObjectIndex < 0)
+        {
+            currentObjectIndex = objetos.Length - 1;
+        }
+        DeactivateAllObjects();
+        objetos[currentObjectIndex].SetActive(true);
+    }
+
+    void DeactivateAllObjects()
+    {
+        foreach (GameObject g in objetos)
+        {
+            g.SetActive(false);
+        }
+    }
+
+}
